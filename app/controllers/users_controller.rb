9a59698a8b7559d33eecec46c67eeee7a1a79171
Robert Pirritano <rpirritano@gmail.com>
2017-05-07 08:13:43 -0700

    Assignment 25 pending completion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
-
+    
     def create
         @user = User.new
         @user.name = params[:user][:name]
@@ -18,7 +18,12 @@ class UsersController < ApplicationController
             render :new
         end
     end
-     
     
-
+    def confirm
+        @user = User.new
+        @user.name = params[:user][:name]
+        @user.email = params[:user][:email]
+        @user.password = params[:user][:password]
+        @user.password_confirmation = params[:user][:password_confirmation]
+    end
 end
